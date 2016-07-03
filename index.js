68e1a26e4c04cf2a5e8137a9401e2f6794911ce9
Emil Persson <emil.persson@creuna.se>
2016-07-03 12:33:48 +0200

    Update and simplify

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-import { pipe, serve } from 'ellie'
+import { serve } from 'ellie'
 
-// Logger middleware, demonstrating a middleware
-// interactive with both the request and response
-const Logger = (next) => async (request) => {
-  const startTime = Date.now()
-  const response = await next(request)
-  const endTime = Date.now()
-  const milliseconds = endTime - startTime
-
-  console.log(
-    new Date(startTime),
-    request.method,
-    request.url,
-    `${milliseconds}ms`
-  )
-
-  return response
-}
-
-// HelloWorld middlware, demonstrating a responding
-// middleware that uses JSX to generate HTML.
 const HelloWorld = () => (request) => (
   <div>
-    Welcome to {request.url}. Enjoy your stay!
+    <h1>Hello World</h1>
+    <pre>{request.method} {request.url}</pre>
   </div>
 )
 
-// Heroku uses this environment variable
+// Heroku uses this port
 const port = process.env.PORT || 8080
 
-// Create the server pipeline
-const server = serve(pipe(
-  Logger,
-  HelloWorld
-))
-
-// Start listening
-server.listen(port).then(() => {
-  console.log(`Listening to port ${port}`)
-})
+serve(HelloWorld)
+  .listen(port)
+  .then(() => console.log(`Listening to port ${port}`))
